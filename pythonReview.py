1a55e7765528cc2690004f79d5c9c7212529bb20
eitansa23-meet <eitaneitan121@gmail.com>
2022-07-25 18:19:41 +0300

    Create pythonReview.py

---
+++ pythonReview.py
@@ -0,0 +1,14 @@
+def create_youtube_video(title,description):
+	dict1 = {"title":title,"description":description,"likes":0,"dislikes":0,"username":"comments"}
+	return dict1
+
+def like(youtube_video):
+	if "likes" in youtube_video:
+		youtube_video["likes"]= likes+1
+		return dict1
+
+def dislike(youtube_video):
+	if "dislikes" in youtube_video:
+		youtube_video["dislikes"]= dislikes+1
+		return dict1
+
