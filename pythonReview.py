f0bb61da93af15419ea3e6c531459e8d37a3cbdc
eitansa23-meet <eitaneitan121@gmail.com>
2022-07-25 19:15:37 +0300

    Update pythonReview.py

--- pythonReview.py
+++ pythonReview.py
@@ -1,5 +1,6 @@
 def create_youtube_video(title,description):
-	dict1 = {"title":title,"description":description,"likes":0,"dislikes":0,"username":"comments"}
+	dict1 = {"title":title,"description":description,"likes":0,"dislikes":0}
+	comments = {}
 	return dict1
 
 def like(youtube_video):
@@ -11,4 +12,16 @@ def dislike(youtube_video):
 	if "dislikes" in youtube_video:
 		youtube_video["dislikes"]= dislikes+1
 		return dict1
+
+def add_comment(youtubevideo,username,comment_text):
+	youtubevideo = dict1
+	comments[username] = comment_text
+
+new_video = create_youtube_video()
+comments(new_video)
+dislikes(new_video)
+likes(new_video)
+print(new_video)
+
+
 
